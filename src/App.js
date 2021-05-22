4d85008e7f9792228bb6c430995326ca3f1c463f
artem <a.4ggame@gmail.com>
2021-05-22 08:49:12 +0300

    fast

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {withRouter} from "react-router";
 import {compose} from "redux";
 import {Routes} from "./components/Routes";
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {YMaps, Map, RoutePanel, Button} from 'react-yandex-maps';
 
 import {configResponsive, useLocalStorageState, useResponsive, useToggle} from 'ahooks';
 
@@ -20,12 +21,48 @@ const App = () => {
     const responsive = useResponsive();
     const [cookie, setCookie] = useLocalStorageState('cookie', false);
     const dispatch = useDispatch();
-  //  const isAuth = useSelector();
+    //  const isAuth = useSelector();
+// https://react-yandex-maps.vercel.app/controls/route-panel
+    //https://www.npmjs.com/package/ymaps-list
+//https://yandex.ru/dev/maps/jsbox/2.1/multiroute_driving
+    function script(url) {
+        if (Array.isArray(url)) {
+            let self = this;
+            let prom = [];
+            url.forEach(function (item) {
+                prom.push(self.script(item));
+            });
+            return Promise.all(prom);
+        }
+
+        return new Promise(function (resolve, reject) {
+            let r = false;
+            let t = document.getElementsByTagName('script')[0];
+            let s = document.createElement('script');
+
+            s.type = 'text/javascript';
+            s.src = url;
+            s.async = true;
+            s.onload = s.onreadystatechange = function () {
+                if (!r && (!this.readyState || this.readyState === 'complete')) {
+                    r = true;
+                    resolve(this);
+                }
+            };
+            s.onerror = s.onabort = reject;
+            t.parentNode.insertBefore(s, t);
+        });
+    }
+
+    script('//api-maps.yandex.ru/2.1/?apikey=106b8cb7-1a46-4beb-9671-03da8c4f3a61&lang=ru_RU').then(() => {
+    });
+    console.log(global.ymaps)
 
     return (
         <div>
             <MainContext.Provider
                 value={cookie}>
+
                 <Routes/>
             </MainContext.Provider>
         </div>
@@ -33,5 +70,5 @@ const App = () => {
 };
 
 export default compose(
-withRouter
+    withRouter
 )(App)
